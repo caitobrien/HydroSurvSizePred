dbfc6605fa8715496dd0d9d1b5d8f3e666a15332
caitobrien <120421572+caitobrien@users.noreply.github.com>
2024-02-07 18:57:52 -0800

    adding pred data

--- R/mod_subpage2_predrisk.R
+++ R/mod_subpage2_predrisk.R
@@ -12,6 +12,13 @@ mod_subpage2_predrisk_ui <- function(id){
   tagList(
     fluidRow(
 
+      shinydashboard::box(
+        width = 12,
+        title = "Predation risk all years",
+        status = "info",
+        plotOutput(outputId = ns("predrisk_plot_allyears"))
+      ),
+
     shinydashboard::box(
       width = 12,
       title = "Predation Risk by year",
@@ -36,7 +43,7 @@ mod_subpage2_predrisk_ui <- function(id){
         step = 1,
         sep = ""),
 
-      plotOutput(outputId = ns("predrisk_plot_allyears"))
+      plotOutput(outputId = ns("predrisk_plot_byyear"))
 
     )
     )
@@ -52,16 +59,32 @@ mod_subpage2_predrisk_server <- function(id){
 
     # Filter data based on slider input
     filtered_data <- reactive({
-      df %>%
+      df_pred_summary %>%
         filter(year >= input$year_slider[1] & year <= input$year_slider[2],
                site %in% c(input$select_loc))
     })
 
-
-    #render plot
+    #render plot all year
     output$predrisk_plot_allyears<- renderPlot(
-      ggplot(filtered_data(), aes(x=site, y=length, color = site))+
+
+
+      filtered_data() %>%
+        ggplot(aes(x = as.factor(year), y = pct_susceptible, color = site))+
+        geom_bar(stat = "identity", position = "dodge", width = .8) +
+        geom_text( aes(label = paste0(round(pct_susceptible,1),"%")),
+                  position = position_fill(vjust = 1)) +
+        facet_wrap(~site, scales = "free") +
+        theme_light() +
+        theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5))
+
+    )
+
+
+    #render plot by year
+    output$predrisk_plot_byyear<- renderPlot(
+      ggplot(filtered_data(), aes(x=site, y=pct_susceptible, color = site))+
         geom_bar(stat = "identity", position = position_dodge()) +
+        scale_y_continuous(labels = scales::percent_format())+
         facet_wrap(~year, ncol = 4) + theme_light()
     )
   })
